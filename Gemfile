f9103ef8e6670fb30ee68cd2f8d4a9bbcd6af4b0
Samuel Kadolph <samuel@kadolph.com>
2014-07-19 21:18:57 -0400

    WIP

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby "2.1.2"
 
 gem "google-api-client", "~> 0.7.1"
 gem "sinatra", "~> 1.4.5", require: "sinatra/base"
+gem "twilio-ruby", "~> 3.11.5"
 gem "unicorn", "~> 4.8.3"
 
 group :development do
--- app.rb
+++ app.rb
@@ -3,20 +3,76 @@ require "sinatra/base"
 
 Bundler.require(:default, Sinatra::Base.environment)
 
-class App < Sinatra::Base
-  set :phone_number, ENV["PHONE_NUMBER"] or raise "PHONE_NUMBER must be set"
+class App < Sinatra::Application
+  set :phone_number, (ENV["PHONE_NUMBER"] or raise "PHONE_NUMBER must be set")
+  set :twilio_account_id, (ENV["TWILIO_ACCOUNT_SID"] or raise "TWILIO_NUMBER must be set")
+  set :twilio_auth_token, (ENV["TWILIO_AUTH_TOKEN"] or raise "TWILIO_NUMBER must be set")
+  set :twilio_number, (ENV["TWILIO_NUMBER"] or raise "TWILIO_NUMBER must be set")
+
+  before do
+    logger.info("Parameters: #{params.inspect}")
+  end
 
   get "/" do
     "Nothing to see here."
   end
 
   post "/call" do
-    content_type "text/xml"
-    "<Response><Dial>#{settings.phone_number}</Dial></Response>"
+    respond do |r|
+      r.Dial(settings.phone_number, callerId: settings.twilio_number)
+      # r.Pause(length: 60)
+      # r.Say("No answer")
+    end
+
+    # call_sid = params["CallSid"]
+    # logger.info("CallSid: #{call_sid}")
+
+    # Thread.start do
+    #   logger.info("wtf")
+    #   sleep 5
+    #   logger.info("wtf")
+
+    #   client = Twilio::REST::Client.new(twilio_account_id, twilio_auth_token)
+    #   call = client.account.calls.get(call_sid)
+    #   logger.info("here?")
+    #   logger.info(call.inspect)
+    #   call.update(method: "POST", url: "https://samuelkadolph-buzz.herokuapp.com/connect")
+    # end
+  end
+
+  post "/connect" do
+    respond do |r|
+      r.Say("IT WORKS!!")
+    end
+  end
+
+  post "/done" do
+    respond do |r|
+    end
   end
 
   post "/sms" do
+    case params["Body"]
+    when /\A\Z/
+    when /\Ahelp\Z/
+      response = "<Insert help here>"
+    else
+      response = "Unrecognized command"
+    end
+
+    respond do |r|
+      r.Message(response)
+    end
+  end
+
+  private
+  # def client
+  #   @client ||= Twilio::REST::Client.new(twilio_account_id, twilio_auth_token)
+  # end
+
+  def respond(&block)
+    status 200
     content_type "text/xml"
-    "<Response></Response>"
+    body Twilio::TwiML::Response.new(&block).text
   end
 end
